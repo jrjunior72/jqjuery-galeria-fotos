9e366e90bca67408488f2704e227bf4f6883a8f5
jrjunior72 <jrjunior72@hotmail.com>
2024-05-27 21:57:00 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImagem = $('#new-image-address').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+        <div class="overlay-image-link">
+            <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(300);
+        $('#new-image-address').val('');
+    })
+})
